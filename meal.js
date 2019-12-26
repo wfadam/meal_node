4616b028aeded70aaa1dde8e77053ffdee553df7
wfadam <wfadamweb@gmail.com>
2019-12-26 14:51:08 +0800

    Update meal.js
    robust to windows hibernate and network issue

--- meal.js
+++ meal.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer');
-const DAY = 24 * 3600 * 1000;
+const HOUR = 3600 * 1000;
+const DAY = 24 * HOUR;
 const dstr = (msec = Date.now()) => new Date(msec).toISOString().substr(0, 10);
 
 async function doNavi(fn, selector, value, navi = false) {
     await this['waitFor'](selector, {visible: true});
     await this[fn](selector, value);
+    //await new Promise((resolve, reject) => setTimeout(_ => resolve(), 100));
     if(navi) await this['waitForNavigation']({waitUntil: 'networkidle0'});
 }
 
@@ -12,6 +14,13 @@ async function submitMeal(mealTime) {
     await doNavi.call(this, 'type', '#cmbMealCode', mealTime, true); 
     await doNavi.call(this, 'type', '#cmbMealName', 'F1:米饭');
     await doNavi.call(this, 'click', '#cmdSubmit', true);
+}
+
+async function checkResult(uid = '', expectedCnt) {
+    await this['waitForFunction']((badgeId, expCnt) => {
+        let trs = document.querySelectorAll('tr[align][style]');
+        return [].filter.call(trs, e => e.innerText.includes(badgeId)).length >= expCnt;
+    }, {}, uid, expectedCnt);
 }
 
 async function displayResult(uid = '') {
@@ -24,32 +33,33 @@ async function displayResult(uid = '') {
 }
 
 async function bookMeal(msec = Date.now()) {
-    let browser;
-    try {
-        browser = await puppeteer.launch({headless: false});
-        const [page] = await browser.pages();
-        await page.goto('http://cvppasip02/SPAS');
-        const frames = await page.frames();
-        await doNavi.call(frames[2], 'type', '#txtPassword', 'password');
-        await doNavi.call(frames[2], 'click', '#cmdLogin', true);
-        await doNavi.call(frames[3], 'click', '#linkMeal');
-        await doNavi.call(frames[3], 'type', '#cmbTheDate', dstr(msec), true);
-        await submitMeal.call(frames[3], 'M1:午餐');
-        await submitMeal.call(frames[3], 'M2:晚餐');
-        const badgeId = await frames[2].$eval('#lblUserInfo', e => e.title);
-        await displayResult.call(frames[3], badgeId);
-    } catch(e) {
-        console.error(e);
-    } finally {
-        browser.close();
-    }
+    let browser = await puppeteer.launch({headless: true});
+    const [page] = await browser.pages();
+    await page.goto('http://cvppasip02/SPAS');
+    const frames = await page.frames();
+    await doNavi.call(frames[2], 'type', '#txtPassword', Buffer.from('Ly9TYW5kaXNrYTh3NGRi', 'base64').toString());
+    await doNavi.call(frames[2], 'click', '#cmdLogin', true);
+    await doNavi.call(frames[3], 'click', '#linkMeal');
+    const badgeId = await frames[2].$eval('#lblUserInfo', e => e.title);
+    await doNavi.call(frames[3], 'type', '#cmbTheDate', dstr(msec), true);
+    await submitMeal.call(frames[3], 'M1:午餐'); await checkResult.call(frames[3], badgeId, 1);
+    await submitMeal.call(frames[3], 'M2:晚餐'); await checkResult.call(frames[3], badgeId, 2);
+    await displayResult.call(frames[3], badgeId);
+    await doNavi.call(frames[3], 'type', '#cmbTheDate', dstr(Date.now()), true);
+    await displayResult.call(frames[3], badgeId);
+    browser.close();
 };
 
 async function batchBook() {
-    const t0 = Date.now();
-    await bookMeal(Date.now() + DAY);
-    console.log(new Date().toLocaleString(), '>', (Date.now() - t0), 'mS');
-    setTimeout(batchBook, DAY);
+    try {
+        const t0 = Date.now();
+        await bookMeal(Date.now() + DAY);
+        console.log(new Date().toLocaleString(), '>', (Date.now() - t0), 'mS');
+    } catch(e) {
+        console.error(e);
+    }
+    setTimeout(batchBook, 4 * HOUR);
+    console.log(`The next round is scheduled at ${new Date(Date.now() + 4 * HOUR).toLocaleString()}`);
 }
 
 if(require.main === module) {
